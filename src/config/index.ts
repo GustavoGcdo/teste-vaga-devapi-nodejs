30e0e7bf4cd8cac64b855a6c29460d5f95e50f29
Gustavo Oliveira <gustavo.gcdo@gmail.com>
2021-04-01 16:47:04 -0400

    add enviroment bd_url for test

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,7 @@
+import dotenvConfig from './dotenv.config';
+
+dotenvConfig();
+
 export default {
   PORT: process.env.PORT || 3000,
   DB_URI: process.env.DB_URI || 'mongodb://localhost/devapi-db',
